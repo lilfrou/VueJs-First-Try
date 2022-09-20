3c304a4ec43c92895b0c54df401cdf7f45264df8
lilfrou <51007021+lilfrou@users.noreply.github.com>
2022-09-20 17:58:26 +0100

    Update test.tf

--- storage/test.tf
+++ storage/test.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.primary.zone_id
   name    = "www.example.com"
-  type    = "A"
+  type        = "A"
   ttl     = 300
   records = [aws_eip.lb.public_ip]
 }
